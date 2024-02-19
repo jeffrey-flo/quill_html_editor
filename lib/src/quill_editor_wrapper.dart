5999a86f29c9671804ea8078c73ad62402434849
jeffrey-flo <jaichang@dreamus.io>
2024-02-19 14:20:13 +0900

    Update quill_editor_wrapper.dart

--- lib/src/quill_editor_wrapper.dart
+++ lib/src/quill_editor_wrapper.dart
@@ -643,7 +643,10 @@ class QuillHtmlEditorState extends State<QuillHtmlEditor> {
    <!DOCTYPE html>
         <html>
         <head>
-        <link href="https://fonts.googleapis.com/css?family=$_encodedStyle:400,400i,700,700i" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=$_encodedStyle:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css?family=$_encodedStyle:400,400i,700,700i" rel="stylesheet"> -->
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">    
         
        <!-- Include the Quill library --> 
